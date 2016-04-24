62cf718277011a2da0d9c29ee5f9c362efd86e30
Jimmy Pocock <jimmypocock@yahoo.com>
2016-04-24 13:38:53 -0500

    Built PC with composition principles.

--- src/com/completeJavaDeveloperCourse/Main.java
+++ src/com/completeJavaDeveloperCourse/Main.java
@@ -3,6 +3,16 @@ package com.completeJavaDeveloperCourse;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Case theCase = new Case("2208", "Dell", "240", new Dimensions(20, 20, 5));
+
+        Monitor monitor = new Monitor("27 inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard motherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, monitor, motherboard);
+
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
     }
 }
